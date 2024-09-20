0ac3956ba5976d8079dfcc154c4a12c9067c5c30
Thiên Phú Nguyễn Trần <12a1nguyentranthienphu2023@gmail.com>
2024-09-21 06:48:51 +0700

    Doc: Add File Bai1.py

---
+++ Bai1.py
@@ -0,0 +1,11 @@
+import numpy
+# a=int(input())
+# b=int(input())
+
+# array=numpy.append([], range(a,b+1))
+# print(array)
+# Chế
+
+
+array=numpy.arange(int(input()), int(input())+1)
+print(array)
